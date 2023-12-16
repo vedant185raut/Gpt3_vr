a571cedb78a9feb4088866571106f79b7665fd39
vedant185raut <vedant185raut@gmail.com>
2023-12-16 13:58:48 +0530

    first commit

---
+++ src/containers/header/Header.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import people from '../../assets/people.png';
+import ai from '../../assets/ai.png';
+import './header.css';
+const Header = () => {
+  return (
+    <div className='gpt3__header section__padding' id='home'>
+      <div className='gpt3__header-content'>
+       <h1 className="gradient__text">Let&apos;s Build Something amazing with GPT-3 OpenAI</h1>
+      <p>Welcome to the future of creativity and collaboration, where GPT-3 OpenAI empowers you to build something truly extraordinary. Unleash your imagination and harness the power of AI to create, innovate, and transform your ideas into reality. Join us on this exciting journey of limitless potential.</p>
+      <div className="gpt3__header-content__input">
+        <input type="email" placeholder="Your Email Address" />
+        <button type="button">Get Started</button>
+      </div>
+      <div className="gpt3__header-content__people">
+        <img src={people} />
+        <p>1,600 people requested access a visit in last 24 hours</p>
+      </div>
+       </div>
+    <div className="gpt3__header-image">
+      <img src={ai} />
+    </div>
+       </div>
+  )
+}
+
+export default Header
